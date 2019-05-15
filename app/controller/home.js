e38a39bbcfadd09c23d424fff7b92b7ff35f6fd5
xiaogliu <vincexgliu@gmail.com>
2019-05-15 21:22:37 +0800

    add mockjs and update readme

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,16 +1,36 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const Mock = require('mockjs');
 
 class HomeController extends Controller {
   // direct call class static method in router
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = '<h1>hi, egg.js</h1>';
   }
   async test() {
     const { ctx } = this;
-    ctx.body = 'hi, test';
+    // this data is mockjs randomly generated
+    const data = Mock.mock({
+      categoryName: '@cname',
+      'categoryId|+1': 10,
+      logo: '@image(64x64,#eee,Logo)',
+      'categoryItems|1-4': [{
+        cname: '@cname',
+        'cid|+1': 100,
+        'item|3-7': [{
+          name: '@cname',
+          'id|+1': 0,
+          link: '@url',
+        }],
+      }],
+      'seller|8-15': [{
+        name: '@cname',
+        url: '@url',
+      }],
+    });
+    ctx.body = data;
   }
 }
 
